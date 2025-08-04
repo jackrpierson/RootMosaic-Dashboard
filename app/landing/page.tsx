68c5edc4725343d213d81056bcecd158889dd049
Jack Pierson <jackpierson3511@gmail.com>
2025-08-04 12:55:31 -0500

    Fix ESLint error: escape apostrophe in landing page

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -211,7 +211,7 @@ export default function LandingPage() {
 
             <div className="mt-6 text-center">
               <p className="text-sm text-gray-600">
-                Don't have an account?{' '}
+                                 Don&apos;t have an account?{' '}
                 <a href="#" className="text-blue-600 hover:text-blue-800">
                   Contact us
                 </a>
